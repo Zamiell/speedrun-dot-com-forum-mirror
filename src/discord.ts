ff5f91cd618a2c6f4154b0d74613ee695bc85765
James <5511220+Zamiell@users.noreply.github.com>
2023-11-28 16:00:09 -0500

    feat: gmail skeleton

--- src/discord.ts
+++ src/discord.ts
@@ -1,26 +1,54 @@
-import { Client } from "discord.js";
+import type { TextChannel } from "discord.js";
+import { ChannelType, Client, GatewayIntentBits } from "discord.js";
+import { assertDefined } from "isaacscript-common-ts";
 import { env } from "./env.js";
 import { logger } from "./logger.js";
+
+let channel: TextChannel | undefined;
 
 export async function discordInit(): Promise<void> {
   const client = new Client({
     // An intent is needed for each type of data that we need Discord to send to us.
-    intents: [],
-    /// intents: [GatewayIntentBits.Guilds],
+    intents: [
+      GatewayIntentBits.Guilds, // Needed for `guild.channels.cache` to be populated.
+      GatewayIntentBits.GuildMessages, // Needed in order to send messages to a text channel.
+    ],
   });
 
   client.on("ready", (c) => {
     logger.info(`Connected to Discord with a username of: ${c.user.username}`);
 
     const guild = c.guilds.cache.get(env.DISCORD_GUILD_ID);
-    console.log("LOL:", guild);
+    assertDefined(
+      guild,
+      `Failed to get the Discord guild corresponding to the guild ID: ${env.DISCORD_GUILD_ID}`,
+    );
+
+    const outputChannel = guild.channels.cache.get(
+      env.DISCORD_OUTPUT_CHANNEL_ID,
+    );
+    assertDefined(
+      outputChannel,
+      `Failed to get the Discord channel corresponding to the channel ID: ${env.DISCORD_OUTPUT_CHANNEL_ID}`,
+    );
+
+    if (outputChannel.type !== ChannelType.GuildText) {
+      throw new TypeError(
+        `The Discord channel corresponding to the channel ID of "${env.DISCORD_OUTPUT_CHANNEL_ID}" is not a text channel.`,
+      );
+    }
+
+    channel = outputChannel;
   });
 
   logger.info("Logging in to Discord...");
-  console.log("LOL:", env.DISCORD_TOKEN);
   await client.login(env.DISCORD_TOKEN);
 }
 
-export function discordSend(): void {
-  /// const channel = await channel.send(msg);
+export async function discordSend(msg: string): Promise<void> {
+  if (channel === undefined) {
+    return;
+  }
+
+  await channel.send(msg);
 }
--- src/gmail.ts
+++ src/gmail.ts
@@ -1,3 +1,176 @@
-export function gmailInit(): void {
-  // TODO
+import { authenticate } from "@google-cloud/local-auth";
+import type { Credentials, OAuth2Client } from "google-auth-library";
+import type { gmail_v1 } from "googleapis";
+import { google } from "googleapis";
+import {
+  fatalError,
+  isFile,
+  readFile,
+  writeFile,
+} from "isaacscript-common-node";
+import { assertDefined, isObject } from "isaacscript-common-ts";
+import fs from "node:fs";
+import path from "node:path";
+import { REPO_ROOT } from "./constants.js";
+import { env } from "./env.js";
+
+const SPEEDRUN_DOT_COM_EMAIL_ADDRESS = "noreply@speedrun.com";
+
+/** If modifying these scopes, delete the token JSON. */
+// eslint-disable-next-line isaacscript/require-capital-const-assertions, isaacscript/require-capital-read-only
+const SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"];
+
+const TOKEN_PATH = path.join(REPO_ROOT, "token.json");
+const CREDENTIALS_PATH = path.join(REPO_ROOT, env.GMAIL_OAUTH_FILENAME);
+
+export async function gmailInit(): Promise<void> {
+  if (!isFile(CREDENTIALS_PATH)) {
+    fatalError(
+      `The "${CREDENTIALS_PATH}" file does not exist. Download the OAuth2 JSON file from Google and put it at this path.`,
+    );
+  }
+
+  const client = await getClient();
+  const gmail = google.gmail({ version: "v1", auth: client });
+  await listLabels(gmail);
 }
+
+async function getClient() {
+  const savedCredentials = getSavedCredentials();
+  if (savedCredentials !== undefined) {
+    return savedCredentials;
+  }
+
+  const client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  setSavedCredentials(client.credentials);
+  return client;
+}
+
+function getSavedCredentials(): OAuth2Client | undefined {
+  if (!fs.existsSync(TOKEN_PATH)) {
+    return undefined;
+  }
+
+  const tokenContent = readFile(TOKEN_PATH);
+  const token = JSON.parse(tokenContent) as unknown;
+  if (!isObject(token)) {
+    throw new Error(
+      `Failed to parse the "${TOKEN_PATH}" file since it was not an object.`,
+    );
+  }
+
+  return google.auth.fromJSON(token) as OAuth2Client;
+}
+
+function setSavedCredentials(newCredentials: Credentials) {
+  const credentialsContent = readFile(CREDENTIALS_PATH);
+  const credentials = JSON.parse(credentialsContent) as unknown;
+  if (!isObject(credentials)) {
+    throw new Error(
+      `Failed to parse the "${CREDENTIALS_PATH}" file since it was not an object.`,
+    );
+  }
+
+  const key = credentials["installed"] ?? credentials["web"] ?? undefined;
+  assertDefined(
+    key,
+    `The "installed" and "web" keys do not exist in the file: ${CREDENTIALS_PATH}`,
+  );
+
+  if (!isObject(key)) {
+    throw new Error(
+      `Failed to parse the "installed" or "web" key in the "${CREDENTIALS_PATH}" file.`,
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { client_id, client_secret } = key;
+
+  if (typeof client_id !== "string") {
+    throw new TypeError(
+      `Failed to parse the "client_id" field in the "${CREDENTIALS_PATH}" file.`,
+    );
+  }
+
+  if (typeof client_secret !== "string") {
+    throw new TypeError(
+      `Failed to parse the "client_secret" field in the "${CREDENTIALS_PATH}" file.`,
+    );
+  }
+
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { refresh_token } = newCredentials;
+
+  if (typeof refresh_token !== "string") {
+    throw new TypeError(
+      `Failed to parse the "refresh_token" field in the "${CREDENTIALS_PATH}" file.`,
+    );
+  }
+
+  const token = {
+    type: "authorized_user",
+    client_id,
+    client_secret,
+    refresh_token: newCredentials.refresh_token,
+  } as const;
+  const tokenContents = JSON.stringify(token, undefined, 2);
+  writeFile(TOKEN_PATH, tokenContents);
+}
+
+async function listLabels(gmail: gmail_v1.Gmail) {
+  const res = await gmail.users.labels.list({
+    userId: "me",
+  });
+  const { labels } = res.data;
+  if (!labels || labels.length === 0) {
+    console.log("No labels found.");
+    return;
+  }
+  console.log("Labels:");
+  for (const label of labels) {
+    console.log(`- ${label.name}`);
+  }
+}
+
+/*
+async function handleNewEmails() {
+  try {
+    // Get the latest emails
+    const listMessagesResponse = await client.users.messages.list({
+      userId: "me",
+      maxResults: 10,
+    });
+
+    // Check if there are any new emails
+    if (
+      listMessagesResponse.data.messages &&
+      listMessagesResponse.data.messages.length > 0
+    ) {
+      for (const messageId of listMessagesResponse.data.messages) {
+        // Get the specific email message
+        const message = await client.users.messages.get({
+          userId: "me",
+          id: messageId,
+        });
+
+        // Check if the email is from the target address
+        if (
+          message.data.payload.headers.find((header) => header.name === "From")
+            .value === targetEmailAddress
+        ) {
+          // Print the debug message
+          console.log(
+            "Debug message: New email received from",
+            targetEmailAddress,
+          );
+        }
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching emails:", error);
+  }
+}
+*/
